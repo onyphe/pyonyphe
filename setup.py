4476e0205c8eac8a07166fb5d46e418c561aebf8
Sebastien Larinier <sebdraven@protonmail.com>
2026-08-01 13:11:16 +0200

    Add compatibility shim package path and tomli dev dependency in pyproject.toml; quote-normalize setup.py strings

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='pyonyphe',
-    version='2.0',
-    packages=['onyphe'],
-    url='https://github.com/sebdraven/pyonyphe',
-    license='MIT',
-    author='Sebastien Larinier',
-    author_email='slarinier@gmail.com',
-    description=''
+    name="pyonyphe",
+    version="2.0",
+    packages=["onyphe"],
+    url="https://github.com/sebdraven/pyonyphe",
+    license="MIT",
+    author="Sebastien Larinier",
+    author_email="slarinier@gmail.com",
+    description="",
 )
